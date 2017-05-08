7e73957e8571d6e0b08b2f8db34ffeb89f8b58a6
leonardo95 <up201305980@fe.up.pt>
2017-05-08 17:09:33 +0100

    changed utils

--- src/utils/utils.java
+++ src/utils/utils.java
@@ -1,5 +1,12 @@
 package utils;
 
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
 public class utils {
 
 	public static int getFirstorSecondHighestValue(int[] proposals, boolean first)
@@ -20,9 +27,40 @@ public class utils {
 			return highest2nd;
 	}
 
-	public static void main(String[] args) {
-		int b[] = {0,10,2,4,7};
+	public static void main(String[] args) throws StaleProxyException {
+		
+		/*int b[] = {0,10,2,4,7};
 		System.out.println(getFirstorSecondHighestValue(b, true));
-		System.out.println(getFirstorSecondHighestValue(b, false));
+		System.out.println(getFirstorSecondHighestValue(b, false));*/
+
+		Runtime rt = Runtime.instance();
+		Profile p = new ProfileImpl();
+
+		//AMS and DF Agents 
+		ContainerController mainContainer = rt.createMainContainer(p);
+
+		//System.out.println("Registered Bidders:");
+		int totalBidders = 3;
+		
+		for (int i = 0; i < totalBidders; i++){
+			Object[] bidderArgs = new Object[1];
+			bidderArgs[0] = "bidder " + i;
+
+			mainContainer.createNewAgent("Bidder " + i, "ContractNetResponderAgent", bidderArgs).start();
+		}
+	
+		//System.out.println("Registered Auctions:");
+		Object[] auctioneerArgs = new Object[totalBidders];
+		
+		for (int i = 0; i < totalBidders; i++){
+			auctioneerArgs[i] = "Bidder " + i;
+		}
+		
+		mainContainer.createNewAgent("Auctioneer 0", "ContractNetInitiatorAgent", auctioneerArgs).start();
+		
+		AgentController gui = mainContainer.createNewAgent("rma", "jade.tools.rma.rma", null);
+		gui.start();
 	}
 }
+
+
